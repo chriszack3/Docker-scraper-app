5c77eaa9f669da2a16543234409ec5fb16688c5a
chriszack3 <czack@chrispc>
2024-09-22 13:03:05 -0400

    dockerfile changes

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@
 FROM czack3/playwright:latest
 WORKDIR /app
 COPY package.json yarn.lock ./
-RUN yarn install --production
+RUN yarn install
+RUN yarn build
 COPY . .
 CMD ["node", "dist/index.js"]
--- index.ts
+++ index.ts
@@ -1,16 +1,59 @@
-import {chromium} from 'playwright';
+import { addExtra } from 'playwright-extra';
+import stealth from 'puppeteer-extra-plugin-stealth';
+import playwright, { Page } from 'playwright';
+import { Cluster } from 'playwright-cluster';
+
+const scrapeGoogle = async (page: Page, search: string) => {
+    await page.setViewportSize({ width: 1920, height: 1080 });
+    await page.goto('https://www.google.com/');
+    await page.fill('textarea', search)
+    await page.keyboard.press('Enter');
+    await page.waitForLoadState('domcontentloaded');
+    await page.getByText("News").first().click()
+    await page.waitForLoadState('networkidle');
+    await page.getByText("Tools").first().click()
+    await page.waitForLoadState('domcontentloaded');
+    await page.getByText("Sorted by relevance").first().click()
+    await page.getByText("Sorted by date").click()
+    //await page.getByText("Recent").first().click()
+    //await page.getByText("Past hour").click()
+    await page.waitForLoadState('networkidle');
+    //const headlines = await page.getByRole("heading").allTextContents()
+    const headlines = await page.$$eval(`div#search div[role="heading"]`, (elements) => elements.map((element) => {
+        const parent = element.parentElement
+        const url = parent?.parentElement?.parentElement?.getAttribute("href")
+        const title = element.textContent
+        const publisher = parent?.firstChild?.textContent
+        const description = parent?.children[2]?.textContent
+        const publishedAgo = parent?.children[4]?.textContent
+        const scrapedAtMS = Date.now()
+
+        return { url, title, publisher, description, publishedAgo, scrapedAtMS, }
+    }));
+    await page.close();
+
+    return headlines;
+}
+
+const chromium = addExtra(playwright.chromium);
+chromium.use(stealth());
 
 (async () => { 
-    const browser = await chromium.launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto('https://www.google.com');
-    console.log(await page.title());
-    console.log('done')
-    await browser.close();
-})()
-
-// import {chromium} from 'playwright';
-
-// console.log('Hello, therwe world!');
-// console.log(chromium);
+    const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_BROWSER,
+        maxConcurrency: 2,
+        playwright: chromium,
+        timeout: 10000,
+        playwrightOptions: {
+            headless: true,
+        }
+    });
+    await cluster.queue(async ({ page }: { page: Page }) => {
+        try {
+            const headlines = await scrapeGoogle(page, 'trump harris second debate');
+            console.log(headlines);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+})();
