d1858625750ad1caaedff5757ec1ab08c19e605b
chriszack3 <czack@chrispc>
2024-09-30 15:14:17 -0400

    builds from typescript now

--- dashboard/client/src/App.tsx
+++ dashboard/client/src/App.tsx
@@ -1,16 +1,21 @@
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import { TodoListCard } from './components/TodoListCard';
-import { Greeting } from './components/Greeting';
 
 function App() {
     return (
         <Container>
             <Row>
-                <Col md={{ offset: 3, span: 6 }}>
-                    <Greeting />
-                    <TodoListCard />
+                <Col>
+                    <h1>
+                        sd
+                        asfasdfsdfs
+                    </h1>
+                </Col>
+                <Col>
+                    <h1>
+                        sdfasdfasdfasdf
+                    </h1>
                 </Col>
             </Row>
         </Container>
